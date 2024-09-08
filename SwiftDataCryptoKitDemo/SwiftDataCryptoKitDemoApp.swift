f4264439ae1c7bc6dbf4268151d725af470adb69
Jonni Åkesson <enterthesource@me.com>
2024-09-09 00:43:12 +0200

    Initial Commit

---
+++ SwiftDataCryptoKitDemo/SwiftDataCryptoKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataCryptoKitDemoApp.swift
+//  SwiftDataCryptoKitDemo
+//
+//  Created by Jonni Akesson on 2024-09-09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataCryptoKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ SwiftDataCryptoKitDemo/View/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  SwiftDataCryptoKitDemo
+//
+//  Created by Jonni Akesson on 2024-09-09.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, HomeView!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
